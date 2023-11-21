dd30ac213509b11935ca4bb54396402cf276b3ca
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-21 13:51:42 +0100

    Tworzenie funkcji

--- CesarCipher/Program.cs
+++ CesarCipher/Program.cs
@@ -20,45 +20,40 @@
 
                 Console.WriteLine("Wpisz zdanie do konwersji:");
                 string? sentence = Console.ReadLine().ToUpper();
+                string result = string.Empty;
 
-                char[] sentenceChars = sentence.ToCharArray();
-                char[] encryptionResult = new char[sentenceChars.Length];
                 if (key.Key == ConsoleKey.D1)
                 {
-                    for (int i = 0; i < sentenceChars.Length; i++)
-                    {
-                        char letter = sentenceChars[i];
-                        if (letter == ' ' || letter == '\n')
-                        {
-                            encryptionResult[i] = ' ';
-                            continue;
-                        }
-
-                        int oldPosition = Array.IndexOf(Alphabet, letter);
-                        int newPosition = (oldPosition + 3) % Alphabet.Length;
-                        encryptionResult[i] = Alphabet[newPosition];
-                    }
+                    result = UseCesarEncryption(sentence, true);
                 }
                 else if (key.Key == ConsoleKey.D2)
                 {
-                    for (int i = 0; i < sentenceChars.Length; i++)
-                    {
-                        char letter = sentenceChars[i];
-                        if (letter == ' ' || letter == '\n')
-                        {
-                            encryptionResult[i] = ' ';
-                            continue;
-                        }
-
-                        int oldPosition = Array.IndexOf(Alphabet, letter);
-                        int newPosition = (oldPosition - 3) % Alphabet.Length;
-                        encryptionResult[i] = Alphabet[newPosition];
-                    }
+                    result = UseCesarEncryption(sentence, false);
                 }
                 Console.WriteLine("Oryginalne zdanie: " + sentence);
-                Console.WriteLine("Zdanie po konwersji: " + string.Join("", encryptionResult));
+                Console.WriteLine("Zdanie po konwersji: " + result);
                 Console.ReadKey();
             }
+        }
+
+        public static string UseCesarEncryption(string? sentence, bool encrypt)
+        {
+            char[] sentenceChars = sentence.ToUpper().ToCharArray();
+            char[] encryptionResult = new char[sentenceChars.Length];
+            for (int i = 0; i < sentenceChars.Length; i++)
+            {
+                char letter = sentenceChars[i];
+                if (letter == ' ' || letter == '\n')
+                {
+                    encryptionResult[i] = ' ';
+                    continue;
+                }
+
+                int oldPosition = Array.IndexOf(Alphabet, letter);
+                int newPosition = encrypt ? (oldPosition + 3) % Alphabet.Length : (oldPosition - 3) % Alphabet.Length;
+                encryptionResult[i] = Alphabet[newPosition];
+            }
+            return string.Join("", encryptionResult);
         }
     }
 }
