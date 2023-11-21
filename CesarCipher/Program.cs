37c294602d30b5fa6c806a58f1e559264f590d53
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-21 13:57:29 +0100

    Dodanie komentarzy

--- CesarCipher/Program.cs
+++ CesarCipher/Program.cs
@@ -8,17 +8,22 @@
         {
             while (true)
             {
+                //Instrukcja
                 Console.Clear();
                 Console.WriteLine("Szyfrowanie przy użyciu szyfru Cezara.");
                 Console.WriteLine("Wybierz jedną z opcji:");
                 Console.WriteLine("\t1 - Zaszyfruj");
                 Console.WriteLine("\t2 - Odszyfruj");
+
+                //Wybór opcji
                 var key = Console.ReadKey();
 
+                //Pobranie zdania do konwersji
                 Console.WriteLine("Wpisz zdanie do konwersji:");
                 string? sentence = Console.ReadLine().ToUpper();
                 string result = string.Empty;
 
+                //Przeprowadzenie szyfrowania
                 if (key.Key == ConsoleKey.D1)
                 {
                     result = Singleton.Perform(sentence, true);
@@ -27,6 +32,8 @@
                 {
                     result = Singleton.Perform(sentence, false);
                 }
+
+                //Podanie wyniku
                 Console.WriteLine("Oryginalne zdanie: " + sentence);
                 Console.WriteLine("Zdanie po konwersji: " + result);
                 Console.ReadKey();
@@ -37,11 +44,20 @@
 
     public class CesarEnryption
     {
+        /// <summary>
+        /// Zawiera litery z całego alfabetu polskiego.
+        /// </summary>
         public static readonly char[] Alphabet = new char[]
         {
             'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż'
         };
 
+        /// <summary>
+        /// Przeprowadza szyfrowanie na podanym zdaniu.
+        /// </summary>
+        /// <param name="sentence">Zdanie do przekonwertowania.</param>
+        /// <param name="encrypt">Jeśli true, zdanie jest szyfrowane. W przeciwnym razie, zdanie jest odszyfrowane.</param>
+        /// <returns>Wynik konwersji.</returns>
         public string Perform(string? sentence, bool encrypt)
         {
             char[] sentenceChars = sentence.ToUpper().ToCharArray();
